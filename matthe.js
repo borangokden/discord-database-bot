018fffd729256bbe0a60b162d352b6f31792e8b3
Matthejs <90551725+Matthejs@users.noreply.github.com>
2021-11-01 19:31:16 +0300

    Update and rename jahky.js to matthe.js

--- matthe.js
+++ matthe.js
@@ -9,12 +9,12 @@ moment.locale("tr")
 let danger = false
 
 client.on("rateLimit", function (RateLimitData) {
-    console.log("RATE LIMIT WARN AZ BEKLE KENDİME GELİYİM AYQ!", RateLimitData)
+    console.log("RATE LIMIT WARN AZ BEKLE KENDİME GELEYİM BRO!", RateLimitData)
 })
 
 client.on("ready", () => {
-    client.user.setPresence({ activity: { name: "Developed by Jahky.", type: "LISTENING" }, status: "dnd" });
-    if (config.channels.VoiceChannelID) client.channels.cache.get(config.channels.VoiceChannelID).join();
+    client.user.setPresence({ activity: { name: (config.bot.Durum), type: "PLAYING" }, status: "online" });
+     if (config.channels.SesKanalıID) client.channels.cache.get(config.channels.SesKanalıID).join();
     if (danger) console.log("Danger mod aktif!")
     if (danger !== true) {
         RoleBackup()
@@ -29,15 +29,15 @@ client.on("message", message => {
     if (!config.bot.owners.includes(message.author.id) || !prefix || !message.guild) return;
     let args = message.content.split(' ').slice(1);
     let command = message.content.split(' ')[0].slice(prefix.length);
-    let embed = new MessageEmbed().setColor(message.member.displayHexColor).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true, })).setFooter("ceki was here!").setTimestamp();
+    let embed = new MessageEmbed().setColor(message.member.displayHexColor).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true, })).setFooter(config.bot.Footer).setTimestamp();
 
     if (command === "backup-al") {
         RoleBackup();
-        message.channel.send(embed.setDescription(`Başarıyla veri tabanına sunucu rol verisini kaydettiniz!`))
+        message.channel.send(embed.setDescription(`Veri tabanı rol verisine kaydedildi.`))
     }
 
     if (command === "eval") {
-        if (!args[0]) return message.channel.send(`Kod belirt!`);
+        if (!args[0]) return message.channel.send(`Kod belirtmelisin!`);
         let code = args.join(' ');
         function clean(text) {
             if (typeof text !== 'string') text = require('util').inspect(text, { depth: 0 })
@@ -47,15 +47,15 @@ client.on("message", message => {
         try {
             var evaled = clean(eval(code));
             if (evaled.match(new RegExp(`${client.token}`, 'g'))) evaled.replace(client.token, "Yasaklı komut");
-            message.channel.send(`${evaled.replace(client.token, "Njk2MTY4Nz8SDIFDU4OTA1MDk4.b4nug3rc3k.bir.t0k3ns4n4cak.kadarsalagim")}`, { code: "js", split: true });
+            message.channel.send(`${evaled.replace(client.token, "ytb matthe tokeni alamazsin oc")}`, { code: "js", split: true });
         } catch (err) { message.channel.send(err, { code: "js", split: true }) };
     };
 
     if (command === "backup-kur") {
         const ıd = args[0]
-            if (!ıd) return message.channel.send(embed.setDescription("rol idsi belirt!"))
+            if (!ıd) return message.channel.send(embed.setDescription("Rol ID'si belirtmelisin."))
         const RoleDatabase = db.get(`rolebackup_${message.guild.id}_${ıd}`);
-      if (!RoleDatabase) return message.channel.send(embed.setDescription("Lütfen geçerli bi rol idsi belirtin!"))
+      if (!RoleDatabase) return message.channel.send(embed.setDescription("Rol ID'si belirtmelisin."))
         const RoleMembers = db.get(`rolemembers_${message.guild.id}_${ıd}`)
         message.guild.roles.create({
             data: {
@@ -66,12 +66,12 @@ client.on("message", message => {
                 permissions: RoleDatabase.permler,
             }
         }).then(newRole => {
-            message.channel.send(embed.setDescription(`Başarıyla ${newRole} (${newRole.id}) rolünün backubını kurdunuz!`))
-            client.channels.cache.get(config.channels.logchannel).send(embed.setDescription(`${message.author} tarafından ${newRole} (${newRole.id}) rolünün backubı kullanıldı!`))
-            if (!RoleMembers) return console.log(`${newRole.name} olayında veri tabanına kayıtlı üye olmadığı için rol dağıtımı iptal edildi!`)
+            message.channel.send(embed.setDescription(`${message.author} yöneticisi **${newRole}** - **${newRole.id}** rolünün datasını kurdu.`))
+            client.channels.cache.get(config.channels.BackupLog).send(embed.setDescription(`${message.author} yöneticisi **${newRole}** - **${newRole.id}** rolünün datasını kurdu.`))
+            if (!RoleMembers) return console.log(`${newRole.name} olayında veri tabanına kayıtlı kullanıcı olmadığı için rol dağıtımı iptal edildi.`)
             RoleMembers.forEach(member => {
-                if (!member) return console.log(`${member.user.username} adlı üyeyi sunucuda bulamadığım için ${newRole.name} rolünü veremedim!`)
-                message.guild.members.cache.get(member).roles.add(newRole.id).then(x => console.log(`${client.users.cache.get(member).username} Adlı üye ${newRole.name} rolünü aldı!`)).catch(err => console.log(`${err} sebebiyle ${client.users.cache.get(member).username} adlı üye ${newRole.name} rolünü alamadı!`))
+                if (!member) return console.log(`${member.user.username} kullanıcısını sunucuda bulamadığım için ${newRole.name} rolü verilemedi.`)
+                message.guild.members.cache.get(member).roles.add(newRole.id).then(x => console.log(`${client.users.cache.get(member).username} kullanıcısı ${newRole.name} rolünü aldı.`)).catch(err => console.log(`${err} sebebiyle ${client.users.cache.get(member).username} kullanıcısı ${newRole.name} rolünü alamadı!`))
             })
         })
 
@@ -80,7 +80,7 @@ client.on("message", message => {
 })
 
 async function RoleBackup() {
-    client.guilds.cache.get(config.Guild.GuildID).roles.cache.filter(e => !e.managed).forEach(async role => {
+    client.guilds.cache.get(config.Guild.SunucuID).roles.cache.filter(e => !e.managed).forEach(async role => {
         db.set(`rolebackup_${role.guild.id}_${role.id}`, {
             name: role.name,
             color: role.hexColor,
@@ -91,12 +91,12 @@ async function RoleBackup() {
         })
         db.set(`rolemembers_${role.guild.id}_${role.id}`, role.members.map(e => e.id))
     })
-    let rolsize = client.guilds.cache.get(config.Guild.GuildID).roles.cache.filter(rls => rls.name !== "@everyone").size
-    console.log(`${moment(Date.now()).format("LLL")} Tarihinde başarıyla ${rolsize} rolün backup alma işlemi gerçekleştirildi!`)
+    let rolsize = client.guilds.cache.get(config.Guild.SunucuID).roles.cache.filter(rls => rls.name !== "@everyone").size
+    console.log(`${moment(Date.now()).format("LLL")} Tarihinde başarıyla ${rolsize} rolün data alma işlemi gerçekleştirildi.`)
 }
 
 client.on("roleDelete", role => {
-    client.channels.cache.get(config.channels.logchannel).send(`@everyone \`${role.name}\` - \`${role.id}\` rolü silindi`)
+    client.channels.cache.get(config.channels.BackupLog).send(`@everyone bir kullanıcı **${role.name}** - **${role.id}** rolünü sildi.`)
 })
 
 client.login(config.bot.token).then(x => console.log(`Bot ${client.user.username} olarak giriş yaptı`)).catch(err => console.log(`Bot Giriş yapamadı! Sebep: ${err}`));
